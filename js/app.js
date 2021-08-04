72a3d50b58f706ba7c5fa26659aa7252977361e2
Adrián <adrianhega.av@gmail.com>
2021-08-04 20:57:31 +0200

    portfolio

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 'use strict'
 
+const tecnologias = document.querySelectorAll('.about__tecnologias')
+console.log(tecnologias)
+
+tecnologias.forEach((v , i) => {
+    
+    window.addEventListener('scroll', () => {
+        let pixel    = window.scrollY
+        let altV     = window.innerHeight
+        let distElem = tecnologias[i].offsetTop
+        console.log(distElem)
+
+        let calculo  = distElem - (altV / 1)
+
+        if (pixel >= calculo) {
+        tecnologias[i].classList.add('activo')
+        }   
+    })
+})
+
